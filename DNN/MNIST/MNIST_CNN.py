c6d109580a5d6656624d6f3f2d8ec222bdf17d64
takahiro <sequence2014@gmail.com>
2017-08-20 23:37:09 +0900

    add inference_function to DNN/MNIST/MNIST_CNN.py #16

--- DNN/MNIST/MNIST_CNN.py
+++ DNN/MNIST/MNIST_CNN.py
@@ -15,6 +15,10 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+# FLAG args
+tf.app.flags.DEFINE_string("run_mode", "inference", "run mode(train or inference)")
+tf.app.flags.DEFINE_string("continuity", True, "continue or from_zero")
+FLAGS = tf.app.flags.FLAGS
 
 #functions
 # 重みの初期化
@@ -37,13 +41,6 @@ def max_pool_2x2(x):
 
 #main function
 if __name__ == '__main__':
-    # ディレクトリの自動生成（すでにある場合は再帰的に消して作成）
-    if tf.gfile.Exists("model/"):
-        tf.gfile.DeleteRecursively("model/")
-        print("removed model dir")
-    tf.gfile.MakeDirs("model/")
-    print("made model dir")
-
     # placeholder(入力データの配列型)、Variable(モデルのパラメータ)の定義
     W = tf.Variable(tf.zeros([784,10]))
     b = tf.Variable(tf.zeros([10]))
@@ -78,29 +75,56 @@ if __name__ == '__main__':
     b_fc2 = bias_variable([10])
     y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
-    # セッションの記述、変数の初期化
-    sess = tf.InteractiveSession()
-
     # 誤差関数の定義、最適化
     cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
     correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    #学習
-    sess.run(tf.global_variables_initializer())
+    # セッションの記述、変数の初期化
+    sess = tf.InteractiveSession()
     saver = tf.train.Saver()
-    for i in range(20001):
-        batch = mnist.train.next_batch(50)
-        if i%100 == 0:
-            train_accuracy = accuracy.eval(feed_dict={
-                x:batch[0], y_: batch[1], keep_prob: 1.0})
-            print("step %d, training accuracy %g"%(i, train_accuracy))
-        if i%5000 == 0 and i>0:
-            saver.save(sess, "model/model_"+str(i)+".ckpt")
 
-        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    # 学習========
+    if FLAGS.run_mode == "train":
+        step = 0
+        if FLAGS.continuity == True:
+            ckpt = tf.train.get_checkpoint_state('./model/')
+            if ckpt: # checkpointがある場合
+                last_model = ckpt.model_checkpoint_path # 最後に保存したmodelへのパス
+                print("load " + last_model)
+                step += int(last_model.split("_")[-1].split(".")[0])
+                saver.restore(sess, last_model) # 変数データの読み込み
+        else:
+            # ディレクトリの自動生成（すでにある場合は再帰的に消して作成）
+            sess.run(tf.global_variables_initializer())
+            if tf.gfile.Exists("model/"):
+                tf.gfile.DeleteRecursively("model/")
+                print("removed model dir")
+            tf.gfile.MakeDirs("model/")
+            print("made model dir")
 
-    # 正答率の出力
-    print("test accuracy %g"%accuracy.eval(feed_dict={
-        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
+        for i in range(step+1,step+20001):
+            batch = mnist.train.next_batch(50)
+            if i%100 == 0:
+                train_accuracy = accuracy.eval(feed_dict={
+                    x:batch[0], y_: batch[1], keep_prob: 1.0})
+                print("step %d, training accuracy %g"%(i, train_accuracy))
+            if i%1000 == 0 and i>0:
+                print("saving model/model_"+str(i)+".ckpt")
+                saver.save(sess, "./model/model_"+str(i)+".ckpt")
+
+            train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+        # 正答率の出力
+        print("test accuracy %g"%accuracy.eval(feed_dict={
+            x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    elif FLAGS.run_mode == "inference":
+        ckpt = tf.train.get_checkpoint_state('./model/')
+        if ckpt: # checkpointがある場合
+            last_model = ckpt.model_checkpoint_path # 最後に保存したmodelへのパス
+            print("load " + last_model)
+            saver.restore(sess, last_model) # 変数データの読み込み
+        print("test accuracy %g"%accuracy.eval(feed_dict={
+            x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
